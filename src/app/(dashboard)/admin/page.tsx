492757d5caf3852071e80ec89d16f6c01c4a6b1a
Petchgnz <phetchgnz@gmail.com>
2025-04-30 00:23:57 +0700

    1.00h

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,16 +1,34 @@
+import AttendanceChart from "@/components/AttendanceChart"
+import CountChart from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row ">
       {/* Left */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* Student Chart */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student"/>
           <UserCard type="teacher"/>
           <UserCard type="parent"/>
           <UserCard type="staff"/>
         </div>
+
+        {/*  Middle Charts */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count Chart */}
+          <div className="w-full h-[450px] lg:w-1/3">
+            <CountChart /> 
+          </div>
+
+          {/* Attendance Chart */}
+          <div className="w-full h-[450px] lg:w-2/3">
+            <AttendanceChart />
+          </div>
+        </div>
+
+        {/* Bottom Charts */}
       </div>
 
 
