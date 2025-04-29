ea7038de80a99ba4e46c30e01a71325cf982a8c7
Petchgnz <phetchgnz@gmail.com>
2025-04-29 07:10:04 +0700

    30:36

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,29 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* Left Part */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link href="/" className="flex items-center justify-center gap-2 lg:justify-start">
+          <Image src="/logo.png" alt="logo" width={32} height={32}/>
+          <span className="hidden lg:block">SchoolLama</span>
+        </Link>
+        <Menu/>
+      </div>
+
+      {/* Right Part */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
