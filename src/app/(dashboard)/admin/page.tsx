b9d4474bc8182cc222b8f77df09b64ce7049a24d
Petchgnz <phetchgnz@gmail.com>
2025-04-29 18:22:58 +0700

    40.00

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,26 @@
+import UserCard from "@/components/UserCard"
+
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
+    <div className="p-4 flex gap-4 flex-col md:flex-row ">
+      {/* Left */}
+      <div className="w-full lg:w-2/3">
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student"/>
+          <UserCard type="teacher"/>
+          <UserCard type="parent"/>
+          <UserCard type="staff"/>
+        </div>
+      </div>
+
+
+
+      {/* Right */}
+      <div className="w-full lg:w-1/3">
+        r
+      </div>
+
+    </div>
   )
 }
 
