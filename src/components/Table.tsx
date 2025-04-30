e93bf4609e6b2b5eeeb968ead3e443e76d506ade
Petchgnz <phetchgnz@gmail.com>
2025-04-30 22:29:29 +0700

    2:06:04 - Creating Teacher Lists

---
+++ src/components/Table.tsx
@@ -0,0 +1,15 @@
+const Table = ({ columns }: { columns: { header: string; accessor: string; className?: string } }) => {
+  return (
+    <table className='w-full mt-4'>
+      <thead className="">
+        <tr className="text-left text-gray-500 text-sm">
+          {columns.map((col) => (
+            <th key={col.accessor}>{col.header}</th>
+          ))}
+        </tr>
+      </thead>
+    </table>
+  )
+}
+
+export default Table
