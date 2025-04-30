ba0d0cbbc61336a0c6a04623efd12061392b4482
Petchgnz <phetchgnz@gmail.com>
2025-04-30 16:14:41 +0700

    1:43 - Finished homepages for each user

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import moment from 'moment'
-import { Calendar, momentLocalizer, View, Views, WorkWeek } from 'react-big-calendar'
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
 import "react-big-calendar/lib/css/react-big-calendar.css"
 import { calendarEvents } from '@/lib/data'
 import { useState } from 'react'
@@ -15,18 +15,18 @@ const BigCalendar = () => {
     setView(selectedView)
   }
   return (
-    <Calendar
-      localizer={localizer}
-      events={calendarEvents}
-      startAccessor="start"
-      endAccessor='end'
-      style={{ height: "98%" }}
-      views={['work_week', 'day']}
-      view={view}
-      onView={handleOnChangeView}
-      min={new Date(2025, 1, 0, 8, 0, 0)}
-      max={new Date(2025, 1, 0, 17, 0, 0)}
-    />
+      <Calendar
+        localizer={localizer}
+        events={calendarEvents}
+        startAccessor="start"
+        endAccessor='end'
+        style={{ height: "98%" }}
+        views={['work_week', 'day']}
+        view={view}
+        onView={handleOnChangeView}
+        min={new Date(2025, 1, 0, 8, 0, 0)}
+        max={new Date(2025, 1, 0, 17, 0, 0)}
+      />
   )
 }
 
