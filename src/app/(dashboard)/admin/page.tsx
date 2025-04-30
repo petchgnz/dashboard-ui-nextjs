67b8f78a1dcef18a6ccd398d6011f8f9a9219716
Petchgnz <phetchgnz@gmail.com>
2025-04-30 14:45:57 +0700

    1:41 Finished student/schedule

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -39,15 +39,8 @@ const AdminPage = () => {
 
       {/* Right */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        {/* Top - Calendar */}
-        <div className="">
           <EventCalendar />
-        </div>
-
-        {/* Bottom - Announcement */}
-        <div className="">
           <Announcements />
-        </div>
       </div>
 
     </div>
